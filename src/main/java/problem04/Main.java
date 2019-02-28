81c9380756bdc116f107a5245069de1aa8d952f2
jo <jo@175.209.90.167>
2019-02-28 10:55:31 +0900

    commit

--- src/main/java/problem04/Main.java
+++ src/main/java/problem04/Main.java
@@ -3,6 +3,11 @@ package problem04;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,9 +16,10 @@ public class Main {
 
 		int[] right = new int[18];
 		Random rd = new Random();
+		int size =0;
 		ArrayList<Double> time = new ArrayList<>();
 		ArrayList<Integer> sucCnt = new ArrayList<>();
-		ArrayList<Double> sum = new ArrayList<>();
+		Map<String, Object> map  = new HashMap<String, Object>();
 		Scanner sc = new Scanner(System.in);
 
 		for(int i=0;i<18;i++) {
@@ -54,16 +60,32 @@ public class Main {
 			long end = System.currentTimeMillis(); //프로그램이 끝나는 시점 계산
 			time.add((( end - start )/1000.0)); 
 			sucCnt.add(success);
-			sum.add((( end - start )/1000.0)+success);
+			map.put(Integer.toString(size++),(( end - start )/1000.0)+success);
 			System.out.println("게임을 계속하겠습니까? y/n");
 			if(sc.nextLine().equalsIgnoreCase("N"))break;
 		}
 		
-		Collections.sort(sum);
-		
-		for(int i=0;i<sum.size();i++) {
-			System.out.println((i+1)+" : "+sucCnt.get(i)+"  /  : "+time.get(i));
-		}
+		Iterator it = sortByValue(map).iterator();
+		int id =1;
+		while(it.hasNext()) {
 
+            int temp = Integer.parseInt((String)it.next());
+            System.out.println((id++)+" : "+sucCnt.get(temp)+"  /  : "+time.get(temp));
+        }
 	}
+	
+	public static List sortByValue(final Map map) {
+
+	        List<String> list = new ArrayList();
+	        list.addAll(map.keySet());
+	        Collections.sort(list,new Comparator() {
+	            public int compare(Object o1,Object o2) {
+	                Object v1 = map.get(o1);
+	                Object v2 = map.get(o2);
+	                return ((Comparable) v2).compareTo(v1);
+
+	            }
+	        });
+	        return list;
+	    }
 }
